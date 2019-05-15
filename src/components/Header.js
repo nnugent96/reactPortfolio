0f148d47483629c1ac0b483a671627fc8ba07450
Nathen Nugent <NNugent@netrixllc.com>
2019-05-15 16:48:05 -0500

    Add music master links

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,15 +6,15 @@ const Header = ({children}) => {
   const style = {
     display: 'inline-block',
     margin: 10,
-    marginBottom: 30,
+    marginBottom: 30
   };
 
   return(
     <div>
       <div>
-        <h3 style={style}><Link to='/'><u>Home</u></Link></h3>
-        <h3 style={style}><Link to='/jokes'><u>Jokes</u></Link></h3>
-        <h3 style={style}><Link to='/music-master'><u>Music Master</u></Link></h3>
+        <h3 style={style}><Link className="linkStyling" to='/'><u>Home</u></Link></h3>
+        <h3 style={style}><Link className="linkStyling" to='/jokes'><u>Jokes</u></Link></h3>
+        <h3 style={style}><Link className="linkStyling" to='/music-master'><u>Music Master</u></Link></h3>
         <p></p>
       </div>
       {children}
